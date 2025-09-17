ff31ceb46fdd3b88c3df483e5b30c274d8eb0697
bhavesh-tech03 <bhaveshpatil992282@gmail.com>
2025-09-17 12:56:48 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-import Card from './components/Card'
-import Counter_class from './components/Counter_class';
 function App() {
-  const [name,setName] = useState(" ");
-  return (
-    <div>
-      {/* <Card title="MyFirstCard1" name={name} setName={setName}/>
-      <Card title="MySecondCard2" name={name} setName={setName}/>
-      <p>I am inside Parent Component and value of name is : {name}</p> */}
 
-      <Counter_class />
+  return (
+    <div className="flex justify-center item-center">
+      <h1 className='font-bold text-red'>Bhavesh</h1>
     </div>
   )
 }
